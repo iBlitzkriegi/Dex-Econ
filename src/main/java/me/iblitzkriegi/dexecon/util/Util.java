0b19154ce8fa1fc0ca023f82c7326114587efd31
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-27 23:28:21 -0500

    Add sendMessag eutil

--- src/main/java/me/iblitzkriegi/dexecon/util/Util.java
+++ src/main/java/me/iblitzkriegi/dexecon/util/Util.java
@@ -1,11 +1,18 @@
 package me.iblitzkriegi.dexecon.util;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Util {
 
     public static String getUniqueId(Player player) {
         return player.getUniqueId().toString();
+    }
+
+    public static void sendMessage(CommandSender commandSender, String message) {
+        //TODO Add plugin chat format here
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 
 }
