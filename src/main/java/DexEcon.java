17a0564d943ebbf9ad09b6e953bd94cc4133ed51
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-27 23:21:11 -0500

    First push

---
+++ src/main/java/DexEcon.java
@@ -0,0 +1,15 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DexEcon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+
+}
