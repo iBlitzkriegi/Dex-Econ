2e40b0d1bf9558bc6673c8201e00bd26293c4a1c
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-27 23:27:19 -0500

    Create util class and add getUniqueId method

---
+++ src/main/java/me/iblitzkriegi/dexecon/util/Util.java
@@ -0,0 +1,11 @@
+package me.iblitzkriegi.dexecon.util;
+
+import org.bukkit.entity.Player;
+
+public class Util {
+
+    public static String getUniqueId(Player player) {
+        return player.getUniqueId().toString();
+    }
+
+}
