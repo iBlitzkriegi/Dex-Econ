09ff54f1d3a7ada5a8579bd05060b8042f57be2c
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2018-12-29 19:07:40 -0500

    Create econ manager class

---
+++ src/main/java/me/iblitzkriegi/dexecon/util/managers/EconManager.java
@@ -0,0 +1,12 @@
+package me.iblitzkriegi.dexecon.util.managers;
+
+import java.util.HashMap;
+
+public class EconManager {
+    public static HashMap<String, Integer> balanceMap = new HashMap<>();
+
+    public static HashMap<String, Integer> getBalanceMap() {
+        return balanceMap;
+    }
+
+}
